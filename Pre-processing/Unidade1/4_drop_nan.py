39df3e154c864a2b240c8496d728c3550f8dce48
aNajujsf <anajuliasilvafreitas20@gmail.com>
2025-04-29 16:04:12 -0300

    atv3

--- Pre-processing/Unidade1/4_drop_nan.py
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -4,3 +4,11 @@ volunteer = load_volunteer_dataset()
 
 ## realize print do dataset volunteer corrigido sem nenhum NAN, para isto removam as colunas NAN e depois as linhas e crie
 #um dataframe novo e print este mostrando a contagem de colunas NAN existentes e mostre também o shape novo.
+# retirando os nan das colunas
+volunteer = volunteer.dropna(axis=1)
+#retirando os nan das linhas
+volunteer = volunteer.dropna()
+#fazendo as contagens de colunas nan
+print(volunteer.isna().sum())
+#mostrando shape
+print(f'esse é o shape = {volunteer.shape}')

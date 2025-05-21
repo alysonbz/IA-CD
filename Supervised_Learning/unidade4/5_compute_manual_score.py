840167b33a10e71d5371a600161c96b4c7cd0765
Erykasousa <eryka.rodrigues@alu.ufc.br>
2025-05-21 19:35:44 -0300

    atv

--- Supervised_Learning/unidade4/5_compute_manual_score.py
+++ Supervised_Learning/unidade4/5_compute_manual_score.py
@@ -2,9 +2,10 @@ import numpy as np
 from src.utils import processing_all_features_sales_clean
 
 def compute_RSS(predictions,y):
-    RSS = None
+    RSS = None np.square(predictions- y)
     return RSS
 def compute_MSE(predictions,y):
+
     MSE= None
     return MSE
 def compute_RMSE(predictions,y):

a2be3750c543c9156bd801916046d07daf5d54d5
jeftedamaceno <jeftedamaceno@gmail.com>
2025-06-06 16:09:24 -0300

    teste

--- Supervised_Learning/unidade4/4_fit_prediction.py
+++ Supervised_Learning/unidade4/4_fit_prediction.py
@@ -4,10 +4,10 @@ from sklearn.model_selection import train_test_split
 sales_df = load_sales_clean_dataset()
 
 # Import mean_squared_error
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, root_mean_squared_error
 
 # Create X and y arrays
-X = sales_df.drop(["sales"], axis=1)
+X = sales_df.drop(["sales", "influencer"], axis=1)
 y = sales_df["sales"].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
@@ -26,7 +26,7 @@ print("Predictions: {}, Actual Values: {}".format(y_pred[:2], y_test[:2]))
 r_squared = reg.score(X_test, y_test)
 
 # Compute RMSE
-rmse = mean_squared_error(y_test, y_pred, squared=False)
+rmse = root_mean_squared_error(y_test, y_pred)
 
 # Print the metrics
 print("R^2: {}".format(r_squared))

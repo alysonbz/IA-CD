0d6b9d27267a50e17d51dcfb3ba8e48c9ac05665
Nivaldo10 <nivaldo@alu.ufc.br>
2025-06-13 15:24:45 -0300

    atualização sobre a Q1

--- AV1/classificacao_q1.py
+++ AV1/classificacao_q1.py
@@ -1,18 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-# Carregando o dataset
+
+# 1.Carregando o dataset
 df = pd.read_csv("C:/Users/jfm12/Downloads/nivaldo/IA-CD/AV1/dataset/chocolate.csv")
 print(df.head(), '\n')
 
-# tratando valores ausentes
+# 2.tratando valores ausentes
 print(df.isnull().sum(), '\n')
-
-categorical_cols = df.select_dtypes(include=['object']).columns
-df[categorical_cols] = df[categorical_cols].fillna("desconhecido")
-df = df.drop(columns=['Bean\nType', 'Broad Bean\nOrigin'])
+# removendo colunas com valores alsentes
+df.dropna(axis=1, inplace=True)
 print(df.isnull().sum())
 
-# Análisando a distribuição da variável-alvo
+# 3.Análisando a distribuição da variável-alvo
 plt.hist(df['Rating'], bins=20, color='darkBlue', edgecolor='black')
 plt.title('Distribuição das Notas (Rating)')
 plt.xlabel('Rating')
@@ -24,16 +23,13 @@ categorical_cols = ['Company \n(Maker-if known)', 'Specific Bean Origin\nor Bar
                     'Cocoa\nPercent', 'Company\nLocation']
 # Aplicar codificação one-hot para as variáveis categóricas
 df_encoded = pd.get_dummies(df, columns=categorical_cols, drop_first=True)
-# Mostrar as primeiras linhas para conferir
 print(df_encoded.head())
-
 
 # 5. Análise estatística exploratória
 print("\nEstatísticas descritivas:")
 print(df_encoded.describe())
 
 # 6. Salvar o arquivo ajustado
-
-
+df_encoded.to_csv("classificacao_ajustado.csv", index=False)
 
 

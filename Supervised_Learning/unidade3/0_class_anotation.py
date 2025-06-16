980eee132da6397afd6a95201b67549fd76aa0ee
Jose Davi <jose.davi@alu.ufc.br>
2025-06-16 14:38:14 -0300

    atv

--- Supervised_Learning/unidade3/0_class_anotation.py
+++ Supervised_Learning/unidade3/0_class_anotation.py
@@ -3,5 +3,3 @@ from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 
 churn_df = load_churn_dataset()
-
-
--- Supervised_Learning/unidade4/0_class_anotation.py
+++ Supervised_Learning/unidade4/0_class_anotation.py
@@ -1,5 +1,3 @@
 from src.utils import load_diabetes_clean_dataset
 
 diabetes_df = load_diabetes_clean_dataset()
-
-

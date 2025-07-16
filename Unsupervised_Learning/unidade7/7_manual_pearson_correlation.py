ff581fe14378d00585ecde885ea777083fb64bbb
aNajujsf <anajuliasilvafreitas20@gmail.com>
2025-07-16 16:59:00 -0300

    q7

--- Unsupervised_Learning/unidade7/7_manual_pearson_correlation.py
+++ Unsupervised_Learning/unidade7/7_manual_pearson_correlation.py
@@ -1,24 +1,28 @@
 # Perform the necessary imports
 import matplotlib.pyplot as plt
-
+import numpy as np
 from src.utils import load_grains_dataset
 
 
 def pearson_correlation(x,y):
-    return None
-
+        soma_xy = np.sum((x - np.mean(x)) * (y - np.mean(y)))
+        soma_x2 = np.sum((x - np.mean(x)) ** 2)
+        soma_y2 = np.sum((y - np.mean(y)) ** 2)
+        r = soma_xy / np.sqrt(soma_x2 * soma_y2)
+        return r
 
 grains_df = load_grains_dataset()
 
 
 # Assign the 0th column of grains: width
-width = __
+width = grains_df.iloc[:,0]
 
 # Assign the 1st column of grains: length
-length = __
+length = grains_df.iloc[:,1]
+
 
 # Calculate the Pearson correlation
-correlation = pearson_correlation(width,length)
+correlation = pearson_correlation(width, length)
 
 # Display the correlation
 print(correlation)

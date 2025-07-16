52adf8c2ed8c97ab3808ce25d287917b942b0fef
aNajujsf <anajuliasilvafreitas20@gmail.com>
2025-07-16 16:52:36 -0300

    q6

--- Unsupervised_Learning/unidade7/6_pearson_correlation.py
+++ Unsupervised_Learning/unidade7/6_pearson_correlation.py
@@ -1,6 +1,6 @@
 # Perform the necessary imports
 import matplotlib.pyplot as plt
-
+from sklearn.decomposition import PCA
 from scipy.stats import pearsonr
 from src.utils import load_grains_dataset
 
@@ -9,18 +9,18 @@ grains_df = load_grains_dataset()
 
 
 # Assign the 0th column of grains: width
-width = __
+width = grains_df.iloc[:,0]
 
 # Assign the 1st column of grains: length
-length = __
+length = grains_df.iloc[:,1]
 
 # Scatter plot width vs length
-plt.scatter(____, ____)
+plt.scatter(width, length)
 plt.axis('equal')
 plt.show()
 
 # Calculate the Pearson correlation
-correlation, pvalue = ____
+correlation, p_value = pearsonr(width, length)
 
 # Display the correlation
 print(correlation)

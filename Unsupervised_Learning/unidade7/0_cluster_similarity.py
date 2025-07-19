4bfbc895bf039319a75b62de732fa6a5a0273e04
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2025-07-19 10:48:00 -0300

    AV2

--- Unsupervised_Learning/unidade7/0_cluster_similarity.py
+++ Unsupervised_Learning/unidade7/0_cluster_similarity.py
@@ -14,8 +14,8 @@ def compute_centroid_linkage(cluster1,cluster2):
     return None
 
 
-cluster1 = [[9.0,8.0],[6.0,4.0],[2.0,10.0],[3.0,6.0],[1.0,0.0]]
-cluster2 = [[7.0,4.0],[1.0,10.0],[6.0,10.0],[1.0,6.0],[7.0,1.0]]
+cluster1 = [[9.0,8.0],[6.0,4.0],[2.0,10.0],[3.0,6.0],[1.0,0.0]] #x1 y1
+cluster2 = [[7.0,4.0],[1.0,10.0],[6.0,10.0],[1.0,6.0],[7.0,1.0]] #x2 y2
 
 print("similaridade ligação simples: ", compute_single_linkage(cluster1,cluster2))
 print("similaridade ligação completa: ", compute_complete_linkage(cluster1,cluster2))

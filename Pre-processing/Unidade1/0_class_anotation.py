461e2f99f48fc65999260be808cfbfa49c90459c
KelseyRicardo <ricardo112.41@gmail.com>
2026-03-18 19:11:17 -0300

    1° questão

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -23,3 +23,6 @@ print("\n",volunteer.info())
 print("\n",volunteer["locality"].isna().sum())
 volunteer_cols = volunteer.drop("Latitude""Longitude")
 print(volunteer_cols)
+
+volunteer_subset = volunteer_cols.dropna(subset=["category_desc"])
+print("\n\nShape: ",volunteer_subset.shape)

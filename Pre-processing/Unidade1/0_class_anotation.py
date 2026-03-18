a5cea891b386760d54cd022be80144ab1eb79bc4
Marcos Matias <marcos.luan.adm.pm@gmail.com>
2026-03-18 16:33:13 -0300

    opa

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -1,5 +1,5 @@
 from src.utils import load_hiking_dataset , load_df2_unidade1,load_wine_dataset, load_df1_unidade1, load_volunteer_dataset
-import pandas as pd
+import pandas
 
 volunteer = load_volunteer_dataset()
 hiking = load_hiking_dataset()
@@ -8,4 +8,14 @@ df1 = load_df1_unidade1()
 df2 = load_df2_unidade1()
 
 
-print(wine.describe())
+#print(wine.describe())
+#print((wine).info())
+#print((df1).info())
+#print(df1.drop([1, 2, 3]))
+#print(df1.drop("A", axis=1))
+#print(df1.isna().sum())
+#print(df1.dropna(subset=["B"]))
+#print(df1.dropna(thresh=2))
+print(df2)
+print(df2.inf())
+df2["C"] = df2["C"].astype("int64")
--- Pre-processing/Unidade1/1_pre_processing.py
+++ Pre-processing/Unidade1/1_pre_processing.py
@@ -3,19 +3,19 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Mostre a dimensão do dataset volunteer
-
+print(volunteer.shape)
 
 #mostre os tipos de dados existentes no dataset
-#print(volunteer.info())
+print(volunteer.info())
 
 #mostre quantos elementos do dataset estão faltando na coluna
 
 
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_cols
+#+volunteer_cols
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_cols
-volunteer_subset =
+#+volunteer_subset =
 
 # Print o shape do subset
 

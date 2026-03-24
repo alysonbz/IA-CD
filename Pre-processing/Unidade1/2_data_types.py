835f4f49ea12fd1043cf68d333112102a238f8df
Abimael-Filho <abimaelfilho@alu.ufc.br>
2026-03-24 10:54:18 -0300

    .

--- Pre-processing/Unidade1/2_data_types.py
+++ Pre-processing/Unidade1/2_data_types.py
@@ -3,13 +3,20 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Print os primeiros elementos da coluna hits
-___
+print(f'-Essas são as primeiras linhas da coluna hits-\n{volunteer['hits'].head()}\n')
 
 # Print as caracteristicas da coluna hits
-__
+print(f'-Essa são as características da coluna hits-'
+      f'\nTipo de coluna: {volunteer['hits'].dtype}'
+      f'\nValores faltando: {volunteer['hits'].isna().sum()}'
+      f'\nResumo geral: {volunteer['hits'].describe()}')
+
 
 # Converta a coluna hits para o tipo int
-___
+volunteer['hits'] = volunteer['hits'].astype('int32')
 
 # Print as caracteristicas da coluna hits novamente
----
+print(f'-Características atualizadas da coluna hits-'
+      f'\nTipo de coluna: {volunteer['hits'].dtype}'
+      f'\nValores faltando: {volunteer['hits'].isna().sum()}'
+      f'\nResumo geral: {volunteer['hits'].describe()}')

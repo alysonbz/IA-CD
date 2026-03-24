a41de977d6fe9803891b6d7921c310777d38e286
Maria <mariadfatima927@gmail.com>
2026-03-24 13:43:36 -0300

    Atividade

--- Pre-processing/Unidade1/2_data_types.py
+++ Pre-processing/Unidade1/2_data_types.py
@@ -3,13 +3,16 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Print os primeiros elementos da coluna hits
-___
+print("\nQ1")
+print(volunteer['hits'].head())
 
 # Print as caracteristicas da coluna hits
-__
+print("\nQ2")
+volunteer[['hits']].info()
 
 # Converta a coluna hits para o tipo int
-___
+volunteer['hits'] = volunteer['hits'].astype(int)
 
 # Print as caracteristicas da coluna hits novamente
----
+print("\nQ4")
+print(volunteer['hits'].head())

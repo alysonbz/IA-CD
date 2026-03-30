7d59d37e55bdb5ec38bf9fd17e1eca8b49de49ba
kaue-cruz123 <kauecruz@alu.ufc.br>
2026-03-30 15:47:07 -0300

    .

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -8,4 +8,17 @@ df1 = load_df1_unidade1()
 df2 = load_df2_unidade1()
 
 
-print(wine.describe())
+print(wine.info()) #informações das colunas
+
+print(df1.drop([1, 2, 3])) #excluir linha
+
+print(df1.drop("C", axis=1)) #excluir coluna
+
+print(df1.isna().sum()) #somar valores ausentes de cada coluna
+
+print(df1.dropna(subset=['A'])) #excluir linhas com valores ausentes
+
+print(df1.dropna(thresh=2)) #excluir as linhas com 2 NaN
+
+print(df2)
+print(df2.info())
--- Pre-processing/Unidade1/1_pre_processing.py
+++ Pre-processing/Unidade1/1_pre_processing.py
@@ -1,23 +1,33 @@
+from pandas import isnull
+
 from src.utils import load_volunteer_dataset
 
 volunteer = load_volunteer_dataset()
 
 # Mostre a dimensão do dataset volunteer
-
+print(volunteer.shape)
 
 #mostre os tipos de dados existentes no dataset
-#print(volunteer.info())
+print(volunteer.info())
 
 #mostre quantos elementos do dataset estão faltando na coluna
-
+print(f'O total de elementos faltantes em Locality é: {volunteer['locality'].isna().sum()}')
 
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_cols
+volunteer_cols = volunteer.drop(columns=['Latitude', 'Longitude'])
+print(volunteer_cols.info())
+
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_cols
-volunteer_subset =
+volunteer_subset = volunteer_cols.dropna(subset=['category_desc'])
 
 # Print o shape do subset
+print(volunteer_subset.shape)
+
+print(volunteer_cols['category_desc'].isnull().sum())
+print(volunteer_subset['category_desc'].isnull().sum())
+
+
 
 
 

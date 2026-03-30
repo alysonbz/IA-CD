7b4035efd9344fcdeb4960d11dcdc478f895e2fa
Maria <mariadfatima927@gmail.com>
2026-03-30 16:49:55 -0300

    Atividade 3

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection import train_test_split
 from src.utils import load_hiking_dataset , load_df2_unidade1,load_wine_dataset, load_df1_unidade1, load_volunteer_dataset
 import pandas as pd
 
@@ -7,5 +8,11 @@ wine  = load_wine_dataset()
 df1 = load_df1_unidade1()
 df2 = load_df2_unidade1()
 
+print(volunteer.head())
 
-print(wine.describe())
+X = volunteer.drop('category_desc', axis = 1)
+y = volunteer['category_desc']
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+print("Conjunto de informações para treino:", y_train.value_counts())
--- Pre-processing/Unidade1/3_training_and_test_sets.py
+++ Pre-processing/Unidade1/3_training_and_test_sets.py
@@ -13,12 +13,11 @@ volunteer = volunteer_new.dropna(subset=['category_desc'])
 print(volunteer_new['category_desc'].value_counts(),'\n','\n')
 
 # Crie um DataFrame com todas as colunas, com exceção de ``category_desc``
-X = volunteer.__(__, a
+X = volunteer.drop('category_desc', axis = 1)
 # Crie um dataframe de labels com a coluna category_desc
-y = __[['__']]
+y = volunteer[['category_desc']]
 
 # # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
-X_train, X_test, y_train, y_test = __(__, __, stratify=__, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
 
 # mostre o balanceamento das classes em 'category_desc' novamente
-___

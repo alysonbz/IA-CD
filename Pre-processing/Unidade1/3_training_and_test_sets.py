86091ca844a0d79ea8dfc44cf138b7831b4b5774
Elyson <perfilelyson@gmail.com>
2026-04-03 16:29:28 -0300

    ok

--- Pre-processing/Unidade1/3_training_and_test_sets.py
+++ Pre-processing/Unidade1/3_training_and_test_sets.py
@@ -1,25 +1,27 @@
 from src.utils import load_volunteer_dataset
-_____
+from sklearn.model_selection import train_test_split
 
 volunteer = load_volunteer_dataset()
 
-# Exclua as colunas Latitude e Longitude de volunteer
-volunteer_new = __
+# 1. Exclua as colunas Latitude e Longitude de volunteer com a função .drop
+volunteer_new = volunteer.drop(columns=['Latitude', 'Longitude'])
 
-# Exclua as linhas com valores null da coluna category_desc de volunteer_new
-volunteer = ___
+# 2. Exclua as linhas com valores null da coluna category_desc de volunteer_new
+volunteer_new = volunteer_new.dropna(subset=['category_desc'])
 
-# mostre o balanceamento das classes em 'category_desc'
-print(___['category_desc'].__,'\n','\n')
+# 3. Mostre o balanceamento das classes em category_desc utilizando a função .value_counts()
+print("\nQuestão 3.")
+print(volunteer_new['category_desc'].value_counts())
 
-# Crie um DataFrame com todas as colunas, com exceção de ``category_desc``
-X = volunteer.__(__, axis=1)
+# 4. Crie um DataFrame com todas as colunas, com exceção de category_desc
+X = volunteer_new.drop('category_desc', axis=1)
 
-# Crie um dataframe de labels com a coluna category_desc
-y = __[['__']]
+# 5. Crie um dataframe de labels com a coluna category_desc
+y = volunteer_new[['category_desc']]
 
-# # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
-X_train, X_test, y_train, y_test = __(__, __, stratify=__, random_state=42)
+# 6. Utilize a amostragem stratificada para separar o dataset em treino e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
 
-# mostre o balanceamento das classes em 'category_desc' novamente
-___
+# 7. Mostre o balanceamento das classes em category_desc novamente
+print("\nQuestão 7.")
+print(y_train['category_desc'].value_counts())

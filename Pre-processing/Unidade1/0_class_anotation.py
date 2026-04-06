5a365ae5f778a61024b771e3262a145149eee6fb
KelseyRicardo <ricardo112.41@gmail.com>
2026-04-06 10:00:16 -0300

    .

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -8,19 +8,14 @@ df1 = load_df1_unidade1()
 df2 = load_df2_unidade1()
 
 
-print(wine.describe())
-print(wine.info())
-print(df1)
-print("\n",df1.dropna())
-print("\n",df1.drop([1,2,4]))
-print("\n",df1.isna().sum())
-print("\n",df1.dropna(subset=["B"]))
-print("\n",df1.dropna(thresh=2))
-
-
-print("\n\nShape: ",volunteer.shape)
-
-print("\n",volunteer.info())
+#print(wine.describe())
+#print(wine.info())
+#print(df1)
+#print("\n",df1.dropna())
+#print("\n",df1.drop([1,2,4]))
+#print("\n",df1.isna().sum())
+#print("\n",df1.dropna(subset=["B"]))
+#print("\n",df1.dropna(thresh=2))
 
 lista=[]
 with open('../dataset/wine.csv', 'r') as f:

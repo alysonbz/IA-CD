555d00cd94a205582cbb562f5bf28ac5af83d789
KelseyRicardo <ricardo112.41@gmail.com>
2026-04-07 20:49:38 -0300

    questão 3

--- Pre-processing/Unidade1/1_pre_processing.py
+++ Pre-processing/Unidade1/1_pre_processing.py
@@ -21,26 +21,7 @@ volunteer_subset = volunteer_cols.dropna(subset=["category_desc"])
 print("\n\nShape: ",volunteer_subset.shape)
 
 # Print o shape do subset
-
-print(wine.describe())
-print(wine.info())
-print(df1)
-print("\n",df1.dropna())
-print("\n",df1.drop([1,2,4]))
-print("\n",df1.isna().sum())
-print("\n",df1.dropna(subset=["B"]))
-print("\n",df1.dropna(thresh=2))
-
-
 print("\n\nShape: ",volunteer.shape)
 
-print("\n",volunteer.info())
 
-print("\n",volunteer["locality"].isna().sum())
-
-volunteer_cols = volunteer.drop(["Latitude","Longitude"],axis=1)
-print(volunteer_cols)
-
-volunteer_subset = volunteer_cols.dropna(subset=["category_desc"])
-print("\n\nShape: ",volunteer_subset.shape)
 

8c7a87b255fe83d14966816601394fd4ed7de7c0
SuianyGomes <suianypinto@gmail.com>
2026-04-08 15:20:09 -0300

    Atividade da unidade 2

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -1,6 +1,6 @@
 from src.utils import load_hiking_dataset , load_df2_unidade1,load_wine_dataset, load_df1_unidade1, load_volunteer_dataset
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 volunteer = load_volunteer_dataset()
 hiking = load_hiking_dataset()
 wine  = load_wine_dataset()
@@ -10,3 +10,12 @@ df2 = load_df2_unidade1()
 
 print(df2)
 print(df2.info())
+
+x = volunteer.drop("category_desc", axis=1)
+y = volunteer["category_desc"]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+#print(y_train.head())
+
+print("Conjunto de Informações para treino:",y_train.value_counts())

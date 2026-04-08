6035a365e0b9d701f4cdb5edef6d31596482d43e
Gabriela <gc171123@gmail.com>
2026-04-08 20:25:46 -0300

    Atividade de casa

--- Pre-processing/Unidade1/5_manual_trainnin_and_test_sets.py
+++ Pre-processing/Unidade1/5_manual_trainnin_and_test_sets.py
@@ -1,30 +1,45 @@
 from src.utils import load_volunteer_dataset
+import pandas as pd
+import numpy as np
 
 volunteer = load_volunteer_dataset()
 
 def train_test_split(X,y,test_size,random_seed=1):
-    #SEU CÓDIGO AQUI
+    np.random.seed(random_seed)
+    target = y.iloc[:, 0]
+    unique_classes = target.unique()
+    train_indices = []
+    test_indices = []
+    for cls in unique_classes:
+        cls_indices = target[target == cls].index.tolist()
+        np.random.shuffle(cls_indices)
+        n_test = int(len(cls_indices) * test_size)
+        test_indices.extend(cls_indices[:n_test])
+        train_indices.extend(cls_indices[n_test:])
+    X_train = X.loc[train_indices]
+    X_test = X.loc[test_indices]
+    y_train = y.loc[train_indices]
+    y_test = y.loc[test_indices]
     return X_train,X_test, y_train, y_test
 
-
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_new = __
+volunteer_new = volunteer.drop(['Latitude', 'Longitude'], axis=1)
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_new
-volunteer = ___
+volunteer = volunteer_new.dropna(subset=['category_desc'])
 
 # mostre o balanceamento das classes em 'category_desc'
-print(___['category_desc'].__,'\n','\n')
+print(volunteer['category_desc'].value_counts(),'\n','\n')
 
 # Crie um DataFrame com todas as colunas, com exceção de ``category_desc``
-X = volunteer.__(__, axis=1)
+X = volunteer.drop('category_desc', axis=1)
 
 # Crie um dataframe de labels com a coluna category_desc
-y = __[['__']]
+y = volunteer[['category_desc']]
 
 # # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
-test_size = ---
+test_size = 0.2
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size,random_seed=1)
 
 # mostre o balanceamento das classes em 'category_desc' novamente
-___
+print(y_train['category_desc'].value_counts())

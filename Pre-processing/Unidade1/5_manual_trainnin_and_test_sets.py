561cf02ebd3401e72f0c4e60773bda31cf7a711c
Marcos Matias <marcos.luan.adm.pm@gmail.com>
2026-04-09 17:35:42 -0300

    Q4

--- Pre-processing/Unidade1/5_manual_trainnin_and_test_sets.py
+++ Pre-processing/Unidade1/5_manual_trainnin_and_test_sets.py
@@ -1,30 +1,47 @@
 from src.utils import load_volunteer_dataset
-
+import numpy as np
+import pandas as pd
 volunteer = load_volunteer_dataset()
 
 def train_test_split(X,y,test_size,random_seed=1):
     #SEU CÓDIGO AQUI
+    np.random.seed(random_seed)
+
+    shuffled_indices = np.random.permutation(len(X))
+
+    test_set_size = int(len(X) * test_size)
+
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+
+    X_train, X_test = X.iloc[train_indices], X.iloc[test_indices]
+    y_train, y_test = y.iloc[train_indices], y.iloc[test_indices]
     return X_train,X_test, y_train, y_test
 
 
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_new = __
+volunteer_new = volunteer.drop(['Latitude', 'Longitude'], axis=1)
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_new
-volunteer = ___
+volunteer = volunteer_new.dropna(subset=['category_desc'])
 
 # mostre o balanceamento das classes em 'category_desc'
-print(___['category_desc'].__,'\n','\n')
+print(volunteer['category_desc'].value_counts(),'\n','\n')
 
 # Crie um DataFrame com todas as colunas, com exceção de ``category_desc``
-X = volunteer.__(__, axis=1)
+X = volunteer.drop('category_desc', axis=1)
 
 # Crie um dataframe de labels com a coluna category_desc
-y = __[['__']]
+y = volunteer['category_desc']
 
 # # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
-test_size = ---
+test_size = 0.2
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size,random_seed=1)
 
 # mostre o balanceamento das classes em 'category_desc' novamente
-___
+print(y_train.value_counts())
+print(y_test.value_counts())
+
+
+#OBS
+#Tive dificuldade e realizar o código, então utlizei IA para me ajudar a realizar esse código, mas também a entender como ele funciona.

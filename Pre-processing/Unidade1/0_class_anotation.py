909781b242bcd340938deabbc30526e1e90c30e9
Gabriela <gc171123@gmail.com>
2026-04-08 21:54:51 -0300

    Exercício

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -1,4 +1,5 @@
 from src.utils import load_hiking_dataset , load_df2_unidade1,load_wine_dataset, load_df1_unidade1, load_volunteer_dataset
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 volunteer = load_volunteer_dataset()
@@ -9,3 +10,22 @@ df2 = load_df2_unidade1()
 
 
 print(wine.describe())
+print(wine.info())
+print(df1)
+print("\n",df1.dropna())
+print("\n",df1.drop([1,2,4]))
+print("\n",df1.isna().sum())
+print("\n",df1.dropna(subset=["B"]))
+print("\n",df1.dropna(thresh=2))
+
+print("\n",df2.info())
+print(df2)
+
+X = volunteer.drop('Category_desc', axis=1)
+y = volunteer['Category_desc']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print("Conjunto de iformações para treino: ", y_train.value_counts())
+
+print("\nConjunto de iformações para treino: ", y_test.value_counts())

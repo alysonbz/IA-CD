74836801ad7f48ccf63dac02eb0c3ffff34b1672
Marcos Matias <marcos.luan.adm.pm@gmail.com>
2026-04-09 08:45:01 -0300

    aj

--- Pre-processing/Unidade2/3_Scaling_data.py
+++ Pre-processing/Unidade2/3_Scaling_data.py
@@ -28,7 +28,7 @@ print('variancia',np.var(X))
 print('variancia do dataset normalizado',np.var(X_norm))
 
 # Divida o dataset em treino e teste com amostragem estratificada
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X_norm, y, stratify=y, random_state=42)
 
 #inicialize o algoritmo KNN
 knn = KNeighborsClassifier()

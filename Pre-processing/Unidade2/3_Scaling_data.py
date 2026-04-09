11352ba16c0e8bae5c1dd2eb50edb1373bca391b
Angelo Neto <angeloneto@alu.ufc.br>
2026-04-09 11:15:03 -0300

    Update 3_Scaling_data.py

--- Pre-processing/Unidade2/3_Scaling_data.py
+++ Pre-processing/Unidade2/3_Scaling_data.py
@@ -8,24 +8,34 @@ import pandas as pd
 
 wine = load_wine_dataset()
 
+# Inicializer o scale
 scaler = StandardScaler()
 
+# exclua do dataset a coluna
 X = wine.drop(['Quality'], axis=1)
 
+#normalize o dataset com scaler
 X_norm = scaler.fit_transform(X)
 
+#obtenha as labels da coluna Quality
 y = wine['Quality'].values
 
+#print a valriância de X
 print('Variancia X:', np.var(X))
 
+#print a variânca do dataset X_norm
 print('Variancia X_norm', np.var(X_norm))
 
+# Divida o dataset em treino e teste com amostragem estratificada
 X_train, X_test, y_train, y_test = train_test_split(
     X_norm, y, stratify=y, random_state=42
 )
 
+#inicialize o algoritmo KNN
 knn = KNeighborsClassifier()
 
+# Aplique a função fit do KNN
 knn.fit(X_train, y_train)
 
+# Verifique o acerto do classificador
 print('score', knn.score(X_test, y_test))

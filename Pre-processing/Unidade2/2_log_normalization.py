5617c2e4fcf3992b2ec300afaac505b64d4e90e3
Angelo Neto <angeloneto@alu.ufc.br>
2026-04-09 10:37:22 -0300

    Update 2_log_normalization.py

--- Pre-processing/Unidade2/2_log_normalization.py
+++ Pre-processing/Unidade2/2_log_normalization.py
@@ -1,19 +1,22 @@
 import numpy as np
+from scipy.stats import describe
+
 from src.utils import load_wine_dataset
 import pandas as pd
 
 wine = load_wine_dataset()
+describe(load_wine_dataset())
 
 pd.set_option('display.max_columns', None)
 
 #print as caractéristicas estatísticas do dataset wine
-print(wine.__)
+print(wine.describe())
 
 ## Aplique a função de nomarlização logarítmica na coluna Proline
-wine[__] = np.__(___)
-#
+wine["Proline"] = np.log("Proline_log")
+
 # Print a variância da coluna proline
-print(___)
+print(np.var["Proline_log"])
 
 # print a variância da coluna proline normalizada
 print(np.var(wine['Proline_log']))

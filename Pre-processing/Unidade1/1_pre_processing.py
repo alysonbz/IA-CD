c5ace4b7522b2d703b29b7864ac4b057bccb6617
kaue-cruz123 <kauecruz@alu.ufc.br>
2026-04-09 23:44:53 -0300

    att

--- Pre-processing/Unidade1/1_pre_processing.py
+++ Pre-processing/Unidade1/1_pre_processing.py
@@ -10,22 +10,20 @@ print(volunteer.shape)
 #mostre os tipos de dados existentes no dataset
 print(volunteer.info())
 
-#mostre quantos elementos do dataset estão faltando na coluna
-print(f'O total de elementos faltantes em Locality é: {volunteer['locality'].isna().sum()}')
+#mostre quantos elementos do dataset estão faltando na coluna locality
+print(f'Valores ausentes na coluna: {volunteer['locality'].isnull().sum()}')
 
 # Exclua as colunas Latitude e Longitude de volunteer
 volunteer_cols = volunteer.drop(columns=['Latitude', 'Longitude'])
-print(volunteer_cols.info())
-
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_cols
-volunteer_subset = volunteer_cols.dropna(subset=['category_desc'])
+volunteer_subset = volunteer_cols.dropna(subset= ['category_desc'])
 
 # Print o shape do subset
 print(volunteer_subset.shape)
 
-print(volunteer_cols['category_desc'].isnull().sum())
-print(volunteer_subset['category_desc'].isnull().sum())
+
+
 
 
 
--- Pre-processing/Unidade1/4_drop_nan.py
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -4,3 +4,25 @@ volunteer = load_volunteer_dataset()
 
 ## realize print do dataset volunteer corrigido sem nenhum NAN, para isto removam as colunas NAN e depois as linhas e crie
 #um dataframe novo e print este mostrando a contagem de colunas NAN existentes e mostre também o shape novo.
+
+import pandas as pd
+from src.utils import load_volunteer_dataset
+
+volunteer = load_volunteer_dataset()
+
+# Remove colunas onde TODOS os valores são NaN
+volunteer_clean = volunteer.dropna(axis=1, how='all')
+
+# Remove colunas com mais de 50% de NaN
+threshold = len(volunteer_clean) * 0.5
+volunteer_clean = volunteer_clean.dropna(axis=1, thresh=int(threshold))
+
+# Remove linhas com qualquer NaN restante
+volunteer_clean = volunteer_clean.dropna(axis=0, how='any')
+
+# Print contagem de NaN (deve ser 0 em tudo)
+print("Contagem de NaN por coluna:")
+print(volunteer_clean.isnull().sum(), '\n')
+
+# Print shape novo
+print("Shape novo:", volunteer_clean.shape)

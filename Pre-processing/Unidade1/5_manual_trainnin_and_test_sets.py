5b20eabb30b2bb84be4dd62962e5cc148108df90
Angelo Neto <angeloneto@alu.ufc.br>
2026-04-13 09:43:13 -0300

    Update 5_manual_trainnin_and_test_sets.py

--- Pre-processing/Unidade1/5_manual_trainnin_and_test_sets.py
+++ Pre-processing/Unidade1/5_manual_trainnin_and_test_sets.py
@@ -3,28 +3,67 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 def train_test_split(X,y,test_size,random_seed=1):
-    #SEU CÓDIGO AQUI
+    
+    np.random.seed(random_seed)
+    
+    #Junta X e y
+    df = pd.concat([X, y], axis=1)
+    
+    train_list = []
+    test_list = []
+    
+    for classe, grupo in df.groupby('category_desc'):
+        
+        #Embaralha os dados da classe
+        grupo = grupo.sample(frac=1, random_state=random_seed)
+        
+        #Embaralha os dados para teste
+        n_test = max(1,int(len(grupo) * test_size))
+        
+        #Divide os dados
+        test = grupo.iloc[:n_test]
+        train = grupo.iloc[n_test:]
+        
+        #Guardar os dados
+        train_list.append(train)
+        test_list.append(test)
+    
+    
+    #Junta Tudo
+    train_df = pd.concat(train_list)
+    test_df = pd.concat(test_list)
+    
+    #Separa X e y nov.
+    X_train = train_df.drop('category_desc', axis=1)
+    y_train = train_df[['category_desc']]
+    
+    X_test = test_df.drop('category_desc', axis=1)
+    y_test = test_df[['category_desc']]
+    
+    
     return X_train,X_test, y_train, y_test
 
 
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_new = __
+volunteer_new = volunteer.drop(['Latitude', 'Longitude'], axis=1)
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_new
-volunteer = ___
+volunteer = volunteer_new.dropna(subset=['category_desc'])
 
 # mostre o balanceamento das classes em 'category_desc'
-print(___['category_desc'].__,'\n','\n')
+print(volunteer['category_desc'].value_counts(), '\n\n')
 
 # Crie um DataFrame com todas as colunas, com exceção de ``category_desc``
-X = volunteer.__(__, axis=1)
+X = volunteer.drop('category_desc', axis=1)
 
 # Crie um dataframe de labels com a coluna category_desc
-y = __[['__']]
+y = volunteer[['category_desc']]
 
 # # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
-test_size = ---
+test_size = 0.2
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size,random_seed=1)
 
 # mostre o balanceamento das classes em 'category_desc' novamente
-___
+print(y_train['category_desc'].value_counts(), '\n\n')
+
+print(y_test['category_desc'].value_counts())

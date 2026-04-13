529f54afeb12164e2a410d7ec757bee6d2349af0
cecilia maria <cecilialima1912@gmail.com>
2026-04-13 10:23:55 -0300

    Concluida unidade 1

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -1,3 +1,5 @@
+from sklearn.model_selection import train_test_split
+
 from src.utils import load_hiking_dataset , load_df2_unidade1,load_wine_dataset, load_df1_unidade1, load_volunteer_dataset
 import pandas as pd
 
@@ -9,3 +11,21 @@ df2 = load_df2_unidade1()
 
 
 print(wine.describe())
+
+
+print(df1.drop([1, 2, 3]))
+
+print(df1.drop("A", axis=1))
+
+print(df1.isna().sum())
+
+print(df1.dropna(subset=["B"]))
+
+print(df1.dropna(thresh=2))
+
+print(df2.info())
+
+x = volunteer.drop('category_desc', axis=1)
+y = volunteer['category_desc']
+
+x_train, x_test, y_train, y_test = train_test_split(  x, y, test_size=0.2, random_state=42)

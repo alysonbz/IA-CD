000e21411cda2f6f705c2ab0f61b8dd8ecb702b3
Dayane Magalhães <dayane.magalhaes@hotmail.com>
2026-04-15 21:16:23 -0300

    Unidade 4 - 3 Questão

--- Supervised_Learning/unidade4/3_visualize_predictions.py
+++ Supervised_Learning/unidade4/3_visualize_predictions.py
@@ -1,17 +1,18 @@
+from scipy.stats import alpha
 
 from src.utils import processing_sales_clean
 # Import matplotlib.pyplot
-import ____.____ as ____
+import matplotlib.pyplot as plt
 
 X,y,predictions = processing_sales_clean()
 
 # Create scatter plot
-plt.scatter(____, ____, color="____")
+plt.scatter(X, y, color="blue", alpha=0.1)
 
 # Create line plot
-plt.plot(____, ____, color="____")
+plt.plot(X, predictions, color="red")
 plt.xlabel("Radio Expenditure ($)")
 plt.ylabel("Sales ($)")
 
 # Display the plot
-plt.____()
+plt.show()

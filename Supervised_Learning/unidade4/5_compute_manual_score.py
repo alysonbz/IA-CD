4763ae83d869e5cd5cc5b1f1dcc7a0e78dd0f5b2
Marcos Matias <marcos.luan.adm.pm@gmail.com>
2026-04-22 16:27:27 -0300

    Q4

--- Supervised_Learning/unidade4/5_compute_manual_score.py
+++ Supervised_Learning/unidade4/5_compute_manual_score.py
@@ -1,6 +1,7 @@
 import numpy as np
 from src.utils import processing_all_features_sales_clean
 
+#fazer manual
 def compute_RSS(predictions,y):
     RSS = None
     return RSS
@@ -8,7 +9,7 @@ def compute_MSE(predictions,y):
     MSE= None
     return MSE
 def compute_RMSE(predictions,y):
-    RMSE = None
+    RMSE = np.sqrt(___)
     return RMSE
 def compute_R_squared(predictions,y):
     r_squared = None
@@ -17,7 +18,7 @@ def compute_R_squared(predictions,y):
 
 X,y,predictions = processing_all_features_sales_clean()
 
-
+#É pra dar igual ao resultado 4_fit_prediction. A 4 é com biblioteca e a 5 é a versão manual.
 print("RSS: {}".format(compute_RSS(predictions,y)))
 print("MSE: {}".format(compute_MSE(predictions,y)))
 print("RMSE: {}".format(compute_RMSE(predictions,y)))

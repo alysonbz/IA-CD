9ae3307818324a5105942ca05e7f213fcba21e5f
Elyson <perfilelyson@gmail.com>
2026-04-27 13:44:11 -0300

    4 e 5

--- Supervised_Learning/unidade4/5_compute_manual_score.py
+++ Supervised_Learning/unidade4/5_compute_manual_score.py
@@ -1,24 +1,28 @@
 import numpy as np
 from src.utils import processing_all_features_sales_clean
 
-def compute_RSS(predictions,y):
-    RSS = None
+def compute_RSS(predictions, y):
+    RSS = np.sum((y - predictions) ** 2)
     return RSS
-def compute_MSE(predictions,y):
-    MSE= None
+
+def compute_MSE(predictions, y):
+    MSE = np.mean((y - predictions) ** 2)
     return MSE
-def compute_RMSE(predictions,y):
-    RMSE = None
+
+def compute_RMSE(predictions, y):
+    RMSE = np.sqrt(np.mean((y - predictions) ** 2))
     return RMSE
-def compute_R_squared(predictions,y):
-    r_squared = None
+
+def compute_R_squared(predictions, y):
+    rss = np.sum((y - predictions) ** 2)
+    tss = np.sum((y - np.mean(y)) ** 2)
+    r_squared = 1 - (rss / tss)
     return r_squared
 
 
-X,y,predictions = processing_all_features_sales_clean()
+X, y, predictions = processing_all_features_sales_clean()
 
-
-print("RSS: {}".format(compute_RSS(predictions,y)))
-print("MSE: {}".format(compute_MSE(predictions,y)))
-print("RMSE: {}".format(compute_RMSE(predictions,y)))
-print("R^2: {}".format(compute_R_squared(predictions,y)))
+print("RSS: {}".format(compute_RSS(predictions, y)))
+print("MSE: {}".format(compute_MSE(predictions, y)))
+print("RMSE: {}".format(compute_RMSE(predictions, y)))
+print("R^2: {}".format(compute_R_squared(predictions, y)))

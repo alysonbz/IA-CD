f129775479d1764dd21d490b5be1d7f01b16e26b
Gabriela <gc171123@gmail.com>
2026-04-27 14:56:47 -0300

    Atividade de casa

--- Supervised_Learning/unidade4/5_compute_manual_score.py
+++ Supervised_Learning/unidade4/5_compute_manual_score.py
@@ -2,16 +2,19 @@ import numpy as np
 from src.utils import processing_all_features_sales_clean
 
 def compute_RSS(predictions,y):
-    RSS = None
+    RSS = np.sum(np.square(y - predictions))
     return RSS
 def compute_MSE(predictions,y):
-    MSE= None
+    MSE = np.mean(np.square(y - predictions))
     return MSE
 def compute_RMSE(predictions,y):
-    RMSE = None
+    RMSE = np.sqrt(np.mean(np.square(y - predictions)))
     return RMSE
 def compute_R_squared(predictions,y):
-    r_squared = None
+    y_mean = np.mean(y)
+    numerator = np.sum(np.square(predictions - y_mean))
+    denominator = np.sum(np.square(y - y_mean))
+    r_squared = numerator / denominator
     return r_squared
 
 

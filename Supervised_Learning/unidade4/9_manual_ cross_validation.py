02e1274dd5864839c62da495c561ac17081a1fe3
Abimael-Filho <abimaelfilho@alu.ufc.br>
2026-05-01 10:58:57 -0300

    ativ

--- Supervised_Learning/unidade4/9_manual_ cross_validation.py
+++ Supervised_Learning/unidade4/9_manual_ cross_validation.py
@@ -9,18 +9,32 @@ class KFold:
 
        self.n_splits = n_splits
 
-   def _compute_score(self,X,y):
-       return None
+   def _compute_score(self, model,X_train, X_test, y_train, y_test):
+       model.fit(X_train, y_train)
+       return model.score(X_test, y_test)
 
-   def cross_val_score(self,obj,X, y):
-
+   def cross_val_score(self,obj, X, y):
         scores = []
 
         # parte 1: dividir o dataset X em n_splits vezes
+        n_samples = len(X)
+        fold_size = n_samples // self.n_splits
+
+        for i in range(self.n_splits):
+            start = i * fold_size
+            end = start + fold_size
+
+            X_test = X[start:end]
+            y_test = y[start:end]
+
+            X_train = np.concatenate((X[:start], X[end:]), axis=0)
+            y_train = np.concatenate((y[:start], y[end:]), axis=0)
 
         # parte 2: Calcular a métrica score para subset dividida na parte 1. Chamar a função _compute_score para cada subset
+        score = self._compute_score(obj, X_train, X_test, y_train, y_test)
 
         #appendar na lista scores cada valor obtido na parte 2
+        scores.append(score)
 
         #parte 3 - retornar a lista de scores
 

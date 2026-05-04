45552af159ae2d00af740dc7bf708e47a24d1ba4
Antonio <anttoniosampaiio@gmail.com>
2026-05-04 15:43:48 -0300

    Atividades da unidade 4

--- Supervised_Learning/unidade4/5_compute_manual_score.py
+++ Supervised_Learning/unidade4/5_compute_manual_score.py
@@ -1,26 +1,46 @@
 import numpy as np
+import numpy as np
 from src.utils import processing_all_features_sales_clean
 
-def compute_RSS(predictions,y):
-    RSS = None
+
+def compute_RSS(predictions, y):
+    erro = predictions - y
+    RSS = np.sum(erro ** 2)
     return RSS
-def compute_MSE(predictions,y):
-    MSE= None
+
+
+def compute_MSE(predictions, y):
+    erro = predictions - y
+    MSE = np.mean(erro ** 2)
     return MSE
-def compute_RMSE(predictions,y):
-    RMSE = None
+
+
+def compute_RMSE(predictions, y):
+    erro = predictions - y
+    MSE = np.mean(erro ** 2)
+    RMSE = np.sqrt(MSE)
     return RMSE
-def compute_R_squared(predictions,y):
-    r_squared = None
+
+
+def compute_R_squared(predictions, y):
+    erro = predictions - y
+
+    RSS = np.sum(erro ** 2)
+
+    media_y = np.mean(y)
+    TSS = np.sum((y - media_y) ** 2)
+
+    r_squared = 1 - (RSS / TSS)
+
     return r_squared
 
 
-X,y,predictions = processing_all_features_sales_clean()
+X, y, predictions = processing_all_features_sales_clean()
 
 
-print("RSS: {}".format(compute_RSS(predictions,y)))
-print("MSE: {}".format(compute_MSE(predictions,y)))
-print("RMSE: {}".format(compute_RMSE(predictions,y)))
-print("R^2: {}".format(compute_R_squared(predictions,y)))
+print("RSS: {}".format(compute_RSS(predictions, y)))
+print("MSE: {}".format(compute_MSE(predictions, y)))
+print("RMSE: {}".format(compute_RMSE(predictions, y)))
+print("R^2: {}".format(compute_R_squared(predictions, y)))
 
 #calcular o RSS de maneira manual
--- Supervised_Learning/unidade4/9_manual_ cross_validation.py
+++ Supervised_Learning/unidade4/9_manual_ cross_validation.py
@@ -1,30 +1,82 @@
 import numpy as np
 from src.utils import load_sales_clean_dataset
-from sklearn.linear_model import LinearRegression
+
+
+class LinearRegressionManual:
+
+    def __init__(self):
+        self.a = 0
+        self.b = 0
+
+    def fit(self, X, y):
+        X = X.flatten()
+
+        x_mean = np.mean(X)
+        y_mean = np.mean(y)
+
+        numerator = np.sum((X - x_mean) * (y - y_mean))
+        denominator = np.sum((X - x_mean) ** 2)
+
+        self.a = numerator / denominator
+        self.b = y_mean - self.a * x_mean
+
+    def predict(self, X):
+        X = X.flatten()
+        return self.a * X + self.b
 
 
 class KFold:
 
-   def __init__(self,n_splits):
+    def __init__(self, n_splits):
+        self.n_splits = n_splits
 
-       self.n_splits = n_splits
+    def _compute_score(self, obj, X, y):
+        obj.fit(X[0], y[0])
 
-   def _compute_score(self,X,y):
-       return None
+        y_pred = obj.predict(X[1])
 
-   def cross_val_score(self,obj,X, y):
+        ss_res = np.sum((y[1] - y_pred) ** 2)
+        ss_tot = np.sum((y[1] - np.mean(y[1])) ** 2)
+
+        score = 1 - (ss_res / ss_tot)
+
+        return score
+
+    def cross_val_score(self, obj, X, y):
 
         scores = []
 
         # parte 1: dividir o dataset X em n_splits vezes
+        tamanho = len(X)
+        indices = np.arange(tamanho)
 
-        # parte 2: Calcular a métrica score para subset dividida na parte 1. Chamar a função _compute_score para cada subset
+        partes = np.array_split(indices, self.n_splits)
 
-        #appendar na lista scores cada valor obtido na parte 2
+        for i in range(self.n_splits):
+            test_index = partes[i]
 
-        #parte 3 - retornar a lista de scores
+            train_index = np.concatenate(
+                [partes[j] for j in range(self.n_splits) if j != i]
+            )
 
-        return scores
+            X_train = X[train_index]
+            X_test = X[test_index]
+
+            y_train = y[train_index]
+            y_test = y[test_index]
+
+            # parte 2: Calcular a métrica score para subset dividida na parte 1. Chamar a função _compute_score para cada subset
+            score = self._compute_score(
+                obj,
+                [X_train, X_test],
+                [y_train, y_test]
+            )
+
+            # append na lista scores cada valor obtido na parte 2
+            scores.append(score)
+
+        # parte 3 - retornar a lista scores
+        return np.array(scores)
 
 
 sales_df = load_sales_clean_dataset()
@@ -33,20 +85,23 @@ sales_df = load_sales_clean_dataset()
 X = sales_df["tv"].values.reshape(-1, 1)
 y = sales_df["sales"].values
 
-# Create a KFold object
+# Create a KFold object
 kf = KFold(n_splits=6)
 
-reg = LinearRegression()
+reg = LinearRegressionManual()
 
 # Compute 6-fold cross-validation scores
-cv_scores = kf.cross_val_score(reg,X, y)
+cv_scores = kf.cross_val_score(reg, X, y)
 
 # Print scores
-print(cv_scores)
+print("Scores:")
+for score in cv_scores:
+    print(f"{score:.6f}")
 
 # Print the mean
-print(np.mean(cv_scores))
+print("\nMean:")
+print(f"{np.mean(cv_scores):.6f}")
 
 # Print the standard deviation
-print(np.std(cv_scores))
-
+print("\nStandard deviation:")
+print(f"{np.std(cv_scores):.6f}")

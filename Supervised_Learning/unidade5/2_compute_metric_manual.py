7274e7690343b2b761f85d6800b4fd9bdaaeb600
Elyson <perfilelyson@gmail.com>
2026-05-06 22:23:35 -0300

    m

--- Supervised_Learning/unidade5/2_compute_metric_manual.py
+++ Supervised_Learning/unidade5/2_compute_metric_manual.py
@@ -1,13 +1,16 @@
 from src.utils import process_diabetes
 
-
 class Metrics:
 
     def __init__(self, y_pred, y_test):
+        self.y_pred = y_pred
+        self.y_test = y_test
+
         self.vp_c1 = 0
         self.vn_c1 = 0
         self.fp_c1 = 0
         self.fn_c1 = 0
+
         self.vp_c0 = 0
         self.vn_c0 = 0
         self.fp_c0 = 0
@@ -15,7 +18,7 @@ class Metrics:
 
     def set_param_classe1(self):
 
-        for yp, yt in zip(y_pred, y_test):
+        for yp, yt in zip(self.y_pred, self.y_test):
             if yp == 1 and yt == 1:
                 self.vp_c1 = self.vp_c1 + 1
             if yp == 1 and yt == 0:
@@ -26,51 +29,71 @@ class Metrics:
                 self.vn_c1 = self.vn_c1 + 1
 
     def set_param_classe2(self):
-        pass
+
+        for yp, yt in zip(self.y_pred, self.y_test):
+            if yp == 0 and yt == 0:
+                self.vp_c0 = self.vp_c0 + 1
+            if yp == 0 and yt == 1:
+                self.fp_c0 = self.fp_c0 + 1
+            if yp == 1 and yt == 0:
+                self.fn_c0 = self.fn_c0 + 1
+            if yp == 1 and yt == 1:
+                self.vn_c0 = self.vn_c0 + 1
 
     def compute_acuraccy(self):
-        return None
+        total = self.vp_c1 + self.vn_c1 + self.fp_c1 + self.fn_c1
+        return (self.vp_c1 + self.vn_c1) / total
 
     def compute_recall_c1(self):
-        return None
+        return self.vp_c1 / (self.vp_c1 + self.fn_c1)
 
     def compute_recall_c0(self):
-        return None
+        return self.vp_c0 / (self.vp_c0 + self.fn_c0)
 
     def compute_precision_c1(self):
-        return None
+        return self.vp_c1 / (self.vp_c1 + self.fp_c1)
 
     def compute_precision_c0(self):
-        return None
+        return self.vp_c0 / (self.vp_c0 + self.fp_c0)
 
     def compute_f1_score_c1(self):
-        return None
+        precision = self.compute_precision_c1()
+        recall = self.compute_recall_c1()
+        return 2 * (precision * recall) / (precision + recall)
 
     def compute_f1_score_c0(self):
-        return None
+        precision = self.compute_precision_c0()
+        recall = self.compute_recall_c0()
+        return 2 * (precision * recall) / (precision + recall)
 
     def compute_confusion_matriz(self):
-        return None
+        return [
+            [self.vp_c1, self.fp_c1],
+            [self.fn_c1, self.vn_c1]
+        ]
 
 
 y_pred, y_test = process_diabetes()
+
 mt = Metrics(y_pred, y_test)
+
 mt.set_param_classe1()
 mt.set_param_classe2()
 
 print("acurácia geral:", mt.compute_acuraccy())
-#
-print("recall classe 0: ")
-#
-print("recall classe 1: ")
-#
-print("precision classe 0: ")
-#
-print("precision classe 1:")
-#
-print("F1-score classe 0:")
-#
-print("F1-score classe 1:")
-#
+
+print("recall classe 0:", mt.compute_recall_c0())
+
+print("recall classe 1:", mt.compute_recall_c1())
+
+print("precision classe 0:", mt.compute_precision_c0())
+
+print("precision classe 1:", mt.compute_precision_c1())
+
+print("F1-score classe 0:", mt.compute_f1_score_c0())
+
+print("F1-score classe 1:", mt.compute_f1_score_c1())
+
 print("Matriz de confusão")
-#
+
+print(mt.compute_confusion_matriz())

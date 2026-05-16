1c72559dbc91522d2659f15fc815feaf4aa8a887
KelseyRicardo <ricardo112.41@gmail.com>
2026-05-16 11:01:32 -0300

    atividade

--- Supervised_Learning/unidade5/2_compute_metric_manual.py
+++ Supervised_Learning/unidade5/2_compute_metric_manual.py
@@ -1,5 +1,5 @@
 from src.utils import process_diabetes
-
+import numpy as np
 
 class Metrics:
 
@@ -25,52 +25,69 @@ class Metrics:
             if yp == 0 and yt == 0:
                 self.vn_c1 = self.vn_c1 + 1
 
-    def set_param_classe2(self):
-        pass
+    def set_param_classe0(self):
+        for yp, yt in zip(y_pred, y_test):
+            if yp == 0 and yt == 0:
+                self.vp_c0 = self.vp_c0 + 1
+            if yp == 0 and yt == 1:
+                self.fp_c0 = self.fp_c0 + 1
+            if yp == 1 and yt == 0:
+                self.fn_c0 = self.fn_c0 + 1
+            if yp == 1 and yt == 1:
+                self.vn_c0 = self.vn_c0 + 1
 
     def compute_acuraccy(self):
-        return None
+        acc = (self.vp_c1+self.vn_c1)/(self.vp_c1+self.vn_c1+self.fp_c1+self.fn_c1)
+        return acc
 
     def compute_recall_c1(self):
-        return None
+        recall = self.vp_c1/(self.vp_c1+self.fn_c1)
+        return recall
 
     def compute_recall_c0(self):
-        return None
+        recall = self.vp_c0 / (self.vp_c0 + self.fn_c0)
+        return recall
 
     def compute_precision_c1(self):
-        return None
+        precision = self.vp_c1/(self.vp_c1+self.fp_c1)
+        return precision
 
     def compute_precision_c0(self):
-        return None
+        precision = self.vp_c0 / (self.vp_c0 + self.fp_c0)
+        return precision
 
     def compute_f1_score_c1(self):
-        return None
+        f1_score = (2*(self.compute_precision_c1()*self.compute_recall_c1()))/(self.compute_precision_c1()+self.compute_recall_c1())
+        return f1_score
 
     def compute_f1_score_c0(self):
-        return None
+        f1_score = (2*(self.compute_precision_c0()*self.compute_recall_c0()))/(self.compute_precision_c0()+self.compute_recall_c0())
+        return f1_score
 
     def compute_confusion_matriz(self):
-        return None
+        matriz = np.array([[self.vp_c1,self.fn_c1],
+                           [self.fp_c1,self.vn_c1]])
+        return matriz
 
 
 y_pred, y_test = process_diabetes()
 mt = Metrics(y_pred, y_test)
 mt.set_param_classe1()
-mt.set_param_classe2()
+mt.set_param_classe0()
 
 print("acurácia geral:", mt.compute_acuraccy())
 #
-print("recall classe 0: ")
+print("recall classe 0: ", mt.compute_recall_c0())
 #
-print("recall classe 1: ")
+print("recall classe 1:", mt.compute_recall_c1())
 #
-print("precision classe 0: ")
+print("precision classe 0: ", mt.compute_precision_c0())
 #
-print("precision classe 1:")
+print("precision classe 1:", mt.compute_precision_c1())
 #
-print("F1-score classe 0:")
+print("F1-score classe 0:", mt.compute_f1_score_c0())
 #
-print("F1-score classe 1:")
+print("F1-score classe 1:", mt.compute_f1_score_c1())
 #
-print("Matriz de confusão")
+print("Matriz de confusão\n", mt.compute_confusion_matriz())
 #

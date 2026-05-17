9094490fe32fcc58484d20ca96f1f9def53bfa3c
Marcos Matias <marcos.luan.adm.pm@gmail.com>
2026-05-16 21:08:48 -0300

    Atv

--- Supervised_Learning/unidade5/2_compute_metric_manual.py
+++ Supervised_Learning/unidade5/2_compute_metric_manual.py
@@ -75,7 +75,6 @@ mt = Metrics(y_pred, y_test)
 mt.set_param_classe1()
 mt.set_param_classe0()
 
-
 print("acurácia geral:", mt.compute_acuraccy())
 #
 print("recall classe 0: ", mt.compute_recall_c0())

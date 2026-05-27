adc3989211ae29ecfc36e06b0cb15758d4dce5c3
Abimael01110000 <abimaelfilho@alu.ufc.br>
2026-05-27 16:40:04 -0300

    atual

--- Supervised_Learning/unidade5/6_hyperparam_tuning_grid_search.py
+++ Supervised_Learning/unidade5/6_hyperparam_tuning_grid_search.py
@@ -1,40 +1,42 @@
 import numpy as np
 
-#import Lasso
-_______
+# import Lasso
+from sklearn.linear_model import Lasso
 
-#import train_test_split
+# import train_test_split
 from sklearn.model_selection import train_test_split
 
-#import kfold
-____
+# import kfold
+from sklearn.model_selection import KFold
 
 # Import GridSearchCV
-______
+from sklearn.model_selection import GridSearchCV
 
 from src.utils import load_diabetes_clean_dataset
 
 diabetes_df = load_diabetes_clean_dataset()
+
 X = diabetes_df.drop(['glucose'],axis=1)
 y = diabetes_df['glucose'].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
 
-#inicialize Lasso
-lasso  = ____
+# initialize Lasso
+lasso = Lasso()
 
-#inicialize kfold
-kf = _____
+# initialize kfold
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
-#Set up the parameter grid
-param_grid = {"____": np.linspace(____, __, ___)}
+# Set up the parameter grid
+param_grid = {"alpha": np.linspace(0.0001, 1, 20)}
 
 # Instantiate lasso_cv
-lasso_cv = ____
+lasso_cv = GridSearchCV(lasso, param_grid, cv=kf)
 
 # Fit to the training data
-___
-
+lasso_cv.fit(X_train, y_train)
 
 print("Tuned lasso paramaters: {}".format(lasso_cv.best_params_))
 print("Tuned lasso score: {}".format(lasso_cv.best_score_))

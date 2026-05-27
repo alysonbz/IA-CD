bb891410751bad936b21dea6d3c63afcafbca36b
Elyson <perfilelyson@gmail.com>
2026-05-27 19:45:59 -0300

    unidade 6 - questões 1, 2 e 3

--- Unsupervised_Learning/unidade6/2_inertia.py
+++ Unsupervised_Learning/unidade6/2_inertia.py
@@ -4,20 +4,20 @@ from src.utils import load_grains_dataset
 from sklearn.cluster import KMeans
 
 samples_df = load_grains_dataset()
-samples = samples_df.drop(['variety','variety_number'],axis=1)
+samples = samples_df.drop(['variety','variety_number'], axis=1)
 
 ks = range(1, 6)
 inertias = []
 
 for k in ks:
     # Create a KMeans instance with k clusters: model
-    ____
+    model = KMeans(n_clusters=k)
 
     # Fit model to samples
-    ____
+    model.fit(samples)
 
     # Append the inertia to the list of inertias
-    ____
+    inertias.append(model.inertia_)
 
 # Plot ks vs inertias
 plt.plot(ks, inertias, '-o')

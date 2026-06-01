bccbba64609ccd086c601a89cc25970c2404dd6f
kaue-cruz123 <kauecruz@alu.ufc.br>
2026-06-01 16:16:35 -0300

    att

--- Supervised_Learning/unidade5/2_compute_metric_manual.py
+++ Supervised_Learning/unidade5/2_compute_metric_manual.py
@@ -4,6 +4,8 @@ from src.utils import process_diabetes
 class Metrics:
 
     def __init__(self, y_pred, y_test):
+        self.y_pred = y_pred
+        self.y_test = y_test
         self.vp_c1 = 0
         self.vn_c1 = 0
         self.fp_c1 = 0
@@ -26,31 +28,52 @@ class Metrics:
                 self.vn_c1 = self.vn_c1 + 1
 
     def set_param_classe2(self):
-        pass
+        for yp, yt in zip(self.y_pred, self.y_test):
+
+            if yp == 0 and yt == 0:
+                self.vp_c0 += 1
+
+            if yp == 0 and yt == 1:
+                self.fp_c0 += 1
+
+            if yp == 1 and yt == 0:
+                self.fn_c0 += 1
+
+            if yp == 1 and yt == 1:
+                self.vn_c0 += 1
 
     def compute_acuraccy(self):
-        return None
+        total = self.vp_c1 + self.vn_c1 + self.fp_c1 + self.fn_c1
+
+        return (self.vp_c1 + self.vn_c1) / total
 
     def compute_recall_c1(self):
-        return None
+        return self.vp_c1 / (self.vp_c1 + self.fn_c1)
 
     def compute_recall_c0(self):
-        return None
+        return self.vp_c0 / (self.vp_c0 + self.fn_c0)
 
     def compute_precision_c1(self):
-        return None
+        return self.vp_c1 / (self.vp_c1 + self.fp_c1)
 
     def compute_precision_c0(self):
-        return None
+        return self.vp_c0 / (self.vp_c0 + self.fp_c0)
 
     def compute_f1_score_c1(self):
-        return None
+        precision = self.compute_precision_c1()
+        recall = self.compute_recall_c1()
+
+        return 2 * (precision * recall) / (precision + recall)
 
     def compute_f1_score_c0(self):
-        return None
+        precision = self.compute_precision_c0()
+        recall = self.compute_recall_c0()
+
+        return 2 * (precision * recall) / (precision + recall)
 
     def compute_confusion_matriz(self):
-        return None
+        return [[self.vp_c1, self.fp_c1],
+                [self.fn_c1, self.vn_c1]]
 
 
 y_pred, y_test = process_diabetes()

47704440cd708ec2510f9a7b59d1b0153d3ac48e
cecilia maria <cecilialima1912@gmail.com>
2026-06-03 16:25:41 -0300

    Concluida 1/2/3/

--- Unsupervised_Learning/unidade7/2_dedogram_movements.py
+++ Unsupervised_Learning/unidade7/2_dedogram_movements.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from docutils.languages.ru import labels
 from scipy.cluster.hierarchy import linkage, dendrogram
 from sklearn.preprocessing import normalize
 from src.utils import load_movements_price_dataset
@@ -8,11 +9,16 @@ movements = movements_df.drop(['company'],axis=1)
 companies = movements_df['company'].values
 
 # Normalize the movements: normalized_movements
-normalized_movements = ____
+normalized_movements = normalize(movements)
 
 # Calculate the linkage: mergings
-mergings = ____
+mergings = linkage(normalized_movements, method='complete')
 
 # Plot the dendrogram
-____
+dendrogram(mergings,
+    labels=companies,
+    leaf_rotation=90,
+    leaf_font_size=6,
+)
+
 plt.show()

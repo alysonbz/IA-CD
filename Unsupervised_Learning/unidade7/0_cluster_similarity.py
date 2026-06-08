4afaaed8722a39cd427356c753cbac71dfdad897
Gabriela <gc171123@gmail.com>
2026-06-08 16:49:53 -0300

    Atividade

--- Unsupervised_Learning/unidade7/0_cluster_similarity.py
+++ Unsupervised_Learning/unidade7/0_cluster_similarity.py
@@ -2,16 +2,21 @@ import numpy as np
 
 
 def compute_single_linkage(cluster1,cluster2):
-     return None
+    distances = [np.linalg.norm(np.array(p1) - np.array(p2)) for p1 in cluster1 for p2 in cluster2]
+    return min(distances)
 
 def compute_complete_linkage(cluster1, cluster2):
-     return None
+    distances = [np.linalg.norm(np.array(p1) - np.array(p2)) for p1 in cluster1 for p2 in cluster2]
+    return max(distances)
 
 def compute_average_linkage(cluster1, cluster2):
-     return None
+    distances = [np.linalg.norm(np.array(p1) - np.array(p2)) for p1 in cluster1 for p2 in cluster2]
+    return sum(distances) / len(distances)
 
 def compute_centroid_linkage(cluster1,cluster2):
-    return None
+    centroid1 = np.mean(cluster1, axis=0)
+    centroid2 = np.mean(cluster2, axis=0)
+    return np.linalg.norm(centroid1 - centroid2)
 
 
 cluster1 = [[9.0,8.0],[6.0,4.0],[2.0,10.0],[3.0,6.0],[1.0,0.0]] #x1 y1

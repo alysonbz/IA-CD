e7a9501216d0dd50008085a35ec0f2f5dc8a8b76
KelseyRicardo <ricardo112.41@gmail.com>
2026-06-15 16:44:23 -0300

    atividade

--- Unsupervised_Learning/unidade7/7_manual_pearson_correlation.py
+++ Unsupervised_Learning/unidade7/7_manual_pearson_correlation.py
@@ -5,17 +5,36 @@ from src.utils import load_grains_dataset
 
 
 def pearson_correlation(x,y):
-    return None
+    x_m = 0
+    for i in range(len(x)):
+        x_m += x[i]
+    x_m = x_m/len(x)
+    y_m = 0
+    for i in range(len(y)):
+        y_m += y[i]
+    y_m = y_m / len(y)
+
+    top = 0
+    botton1 = 0
+    botton2 = 0
+    for i in range(len(x)):
+        top += (x[i]-x_m)*(y[i]-y_m)
+        botton1 += (x[i]-x_m)**2
+        botton2 += (y[i]-y_m)**2
+    botton = (botton1*botton2)**(1/2)
+    r = top/botton
+
+    return r
 
 
 grains_df = load_grains_dataset()
 
 
 # Assign the 0th column of grains: width
-width = __
+width = grains_df['0']
 
 # Assign the 1st column of grains: length
-length = __
+length = grains_df['1']
 
 # Calculate the Pearson correlation
 correlation = pearson_correlation(width,length)

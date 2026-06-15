327cece1db91bf6962db581c54458ff56b839add
Marcos Matias <marcos.luan.adm.pm@gmail.com>
2026-06-15 16:18:05 -0300

    atv

--- Unsupervised_Learning/unidade7/6_pearson_correlation.py
+++ Unsupervised_Learning/unidade7/6_pearson_correlation.py
@@ -9,10 +9,10 @@ grains_df = load_grains_dataset()
 
 
 # Assign the 0th column of grains: width
-width = __
+width = grains_df[:, 0]
 
 # Assign the 1st column of grains: length
-length = __
+length = grains_df[:, 1]
 
 # Scatter plot width vs length
 plt.scatter(____, ____)

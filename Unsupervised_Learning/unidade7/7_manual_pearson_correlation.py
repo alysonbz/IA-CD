afb44e951bc5abe3fe6ac6e962a2ee487192c42f
Marcos Matias <marcos.luan.adm.pm@gmail.com>
2026-06-15 17:11:08 -0300

    atv

--- Unsupervised_Learning/unidade7/7_manual_pearson_correlation.py
+++ Unsupervised_Learning/unidade7/7_manual_pearson_correlation.py
@@ -1,6 +1,6 @@
 # Perform the necessary imports
 import matplotlib.pyplot as plt
-
+import numpy as np
 from src.utils import load_grains_dataset
 
 
@@ -12,13 +12,29 @@ grains_df = load_grains_dataset()
 
 
 # Assign the 0th column of grains: width
-width = __
+width = grains_df['0']
 
 # Assign the 1st column of grains: length
-length = __
+length = grains_df['1']
+
+# Scatter plot width vs length
+plt.scatter(width, length)
+plt.axis('equal')
+plt.show()
+
 
 # Calculate the Pearson correlation
-correlation = pearson_correlation(width,length)
+def pearson(x, y):
+    media_x = np.mean(x)
+    media_y = np.mean(y)
+    nume = np.sum((x-media_x)*(y-media_y))
+    deno_x = np.sum((x-media_x)**2)
+    deno_y = np.sum((y-media_y)**2)
+    deno = np.sqrt(deno_x*deno_y)
+
+    return nume/deno
+
+correlation = pearson(width,length)
 
 # Display the correlation
 print(correlation)

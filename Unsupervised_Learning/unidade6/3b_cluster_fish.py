d87d2bede873e737ded840ffae1833afed50b390
Marcos Matias <marcos.luan.adm.pm@gmail.com>
2026-06-23 12:55:17 -0300

    atv

--- Unsupervised_Learning/unidade6/3b_cluster_fish.py
+++ Unsupervised_Learning/unidade6/3b_cluster_fish.py
@@ -7,14 +7,20 @@ samples = samples_df.drop(['specie'],axis=1)
 specie = samples_df['specie'].values
 
 # Create KMeans instance: kmeans with 4 custers
-
+kmeans = KMeans(n_clusters=4, random_state=42)
+kmeans.fit(samples)
 
 # Use fit_predict to fit model and obtain cluster labels: labels
-
+labels = kmeans.fit_predict(samples)
 
 # Create a DataFrame with labels and varieties as columns: df
-
+df = pd.DataFrame({
+    'Labels': labels,
+    'Varieties': specie
+})
 
 # Create crosstab: ct
+ct = pd.crosstab(df['Labels'], df['Varieties'])
 
 # Display ct
+print(ct)

faec2be7a377ad6abe98a29036e65c116f389d0a
Antonio <anttoniosampaiio@gmail.com>
2026-06-25 10:41:25 -0300

    Atvds

--- Unsupervised_Learning/unidade6/3b_cluster_fish.py
+++ Unsupervised_Learning/unidade6/3b_cluster_fish.py
@@ -3,19 +3,23 @@ from src.utils import load_fish_dataset
 from sklearn.cluster import KMeans
 
 samples_df = load_fish_dataset()
-samples = samples_df.drop(['specie'],axis=1)
+samples = samples_df.drop(['specie'], axis=1)
 specie = samples_df['specie'].values
 
-# Create KMeans instance: kmeans with 4 custers
-
+# Create KMeans instance: kmeans with 4 clusters
+kmeans = KMeans(n_clusters=4, random_state=42)
 
 # Use fit_predict to fit model and obtain cluster labels: labels
-
+labels = kmeans.fit_predict(samples)
 
 # Create a DataFrame with labels and varieties as columns: df
-
+df = pd.DataFrame({
+    'labels': labels,
+    'specie': specie
+})
 
 # Create crosstab: ct
-
+ct = pd.crosstab(df['labels'], df['specie'])
 
 # Display ct
+print(ct)
